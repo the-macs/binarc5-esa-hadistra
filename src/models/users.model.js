4fc3f2c622382fcf225b12e35ca5f8f191dfda35
the-macs <hadistraesa09@gmail.com>
2022-08-08 10:25:30 +0700

    Delete User Default User

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,10 +1,10 @@
 const users = [
-    {
-        id: 'a3d2fa70-16c8-11ed-861d-0242ac120002',
-        username: 'esa.hadistra',
-        password: 'password',
-        name: 'Esa Hadistra'
-    }
+    // {
+    //     id: 'a3d2fa70-16c8-11ed-861d-0242ac120002',
+    //     username: 'esa.hadistra',
+    //     password: 'password',
+    //     name: 'Esa Hadistra'
+    // }
 ]
 
 module.exports = users
