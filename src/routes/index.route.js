605d717787b47601f045fd2b6cdbe4cacc0537de
IT Megasetia <it.megasetia@gmail.com>
2022-08-14 16:37:18 +0700

    Delete useless script

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const routerRoot = express()
+const router = express.Router()
 
 const userRoute = require('./user.route')
 const commonRoute = require('./common.route')
 const apiUserRoute = require('./api/user.route')
 
+router.use(userRoute)
+router.use(commonRoute)
+router.use(apiUserRoute)
 
-routerRoot.use(userRoute)
-routerRoot.use(commonRoute)
-routerRoot.use(apiUserRoute)
-
-module.exports = routerRoot
+module.exports = router
